4ac2614c5a0567d584349bdca2f33aa112edade8
SM1LE21 <lasknut@gmail.com>
2023-01-20 08:21:22 +0100

    box-shadow update for input + textarea

--- src/components/CreateTicket.jsx
+++ src/components/CreateTicket.jsx
@@ -65,7 +65,7 @@ const CreateTicket = ({ clientId = '' , handleButtonPress} = {}) => {
               <br />
               <label htmlFor="body">Body:</label>
               <textarea
-              style={{borderRadius: '12px'}}
+                style={{borderRadius: '12px', boxShadow: 'rgba(0, 0, 0, .2) 0 3px 5px -1px,rgba(0, 0, 0, .14) 0 6px 10px 0,rgba(0, 0, 0, .12) 0 1px 18px 0'}}
                 id="body"
                 value={body}
                 onChange={(e) => setBody(e.target.value)}
